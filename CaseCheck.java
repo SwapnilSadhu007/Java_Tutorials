34c7c0b96e375dfbd4d3b9bf8da30cbce7758310
Swapnil Sadhu <jit.sadhu007@gmail.com>
2026-05-21 20:52:53 +0530

    CaseCheck.java

---
+++ CaseCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().trim();
+        int n = sc.nextInt();
+        char ch = str.charAt(n);
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("Uppercase");
+        } else {
+            System.out.println("Not an alphabet");
+        }
+    }
+}
