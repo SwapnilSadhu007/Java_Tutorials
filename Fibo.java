1168398c3a37bc0fdf2c65b0f325294500a65fb8
Swapnil Sadhu <jit.sadhu007@gmail.com>
2026-05-22 20:59:25 +0530

    Fibo.java

--- Fibo.java
+++ Fibo.java
@@ -3,6 +3,7 @@ class Fibo{
 public static void main(String[] args){
 Scanner in = new Scanner(System.in);
 int n = in.nextInt();
+System.out.println("Range:"+n);
 int a= 0;
 int b=1;
 System.out.println(a+ " ");
